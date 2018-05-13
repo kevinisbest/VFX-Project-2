b58920a8a5544dcd0a6cbc2ee1052b306b6fb15e
victor30518 <victor30518@gmail.com>
2018-05-14 05:49:23 +0800

    Update Code

--- src/ReadImages.m
+++ src/ReadImages.m
@@ -13,30 +13,30 @@ function [images] = ReadImages(folder, extension)
     img_count = length(files);
     
     img = imread(filename);
-    img = imrotate(img, 270);
+    % img = imrotate(img, 270);
     img_height = size(img,1);
     img_width = size(img,2);
     img_channel = size(img,3);
     disp([img_height,img_width]);
     n=0;
-    while(img_height>600)
+    while(img_height>1000)
         img_height = img_height/2;
         img_width = img_width/2;
         n = n+1
     end
     disp([img_height,img_width]);
     images = zeros(img_height, img_width, img_channel, img_count, 'uint8');
-     for i = 1:img_count
-         filename = [folder, '/', files(i).name];
-%          disp(filename);
-         img = imread(filename);
-         img = imrotate(img, 270);
-         j = n;
-         while(j>0)
+    for i = 1:img_count
+        filename = [folder, '/', files(i).name];
+        disp(filename);
+        img = imread(filename);
+        % img = imrotate(img, 270);
+        j = n;
+        while(j>0)
             img = imresize(img,0.5);
             j = j-1;
-         end
-         images(:,:,:,i) = img;
-     end
-    
+        end
+        images(:,:,:,i) = img;
+    end
+    imshow(images(:,:,:,2));
 end
